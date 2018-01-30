03fa8cdfbc950427e1f04a5ffd1d5b6b457f78d4
ThomasLengeling <thomas@codigogenerativo.com>
2018-01-30 11:17:14 -0500

    files

---
+++ color segmentation.md
@@ -0,0 +1,5 @@
+
+
+Image segmentation using Texture and Color features in C++
+
+https://github.com/ankitdhall/imageSegmentation
---
+++ color_space.py
@@ -0,0 +1,5 @@
+#https://www.learnopencv.com/color-spaces-in-opencv-cpp-python/
+
+
+#Color spaces between
+# HSV and LAB Color-Space
